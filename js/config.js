e3f2bd7b023749124767c5423a9beb16ee152851
Erik Fernandes <gamermito222@gmail.com>
2026-09-26 10:36:06 -0300

    contato com a almeidasnet

--- js/config.js
+++ js/config.js
@@ -35,7 +35,7 @@ window.ALMEIDASNET_CONFIG = {
   whatsappMensagemPadrao: "Olá! Quero contratar um plano da AlmeidasNet",
 
   // Botão de telefone flutuante e textos "ligue para"
-  telefone: "(71) 3019-5811",
+  telefone: "(71) 99381-2371",
   telefoneLink: "tel:30195811",
 
   endereco: "Tv. Domingos Silva, 35, Itapuã, Salvador - BA",
